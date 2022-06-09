e28dc83ea132b691cae7037c10af2c1c8bf0382c
shockylemi <shockylemi@gmail.com>
2022-06-09 12:59:56 +0430

    CameraService definition and usage

--- WebController/Services/CameraService.cs
+++ WebController/Services/CameraService.cs
@@ -9,14 +9,16 @@ namespace WebController.Services
 {
     public class CameraService
     {
+        public List<Camera> Cameras = new List<Camera>();
+        public Camera? MainCamera { get => this.Cameras.First(); }
+
+        public string ImageSaveDirectory = Directory.GetCurrentDirectory();
+
+
         CanonAPI APIHandler = new CanonAPI();
 
-        List<Camera> Cameras = new List<Camera>();
-        Camera? MainCamera { get => this.Cameras.First(); }
-
-        string ImageSaveDirectory = Directory.GetCurrentDirectory();
-
         bool Error = false;
+
         ManualResetEvent WaitEvent = new ManualResetEvent(false);
 
         public CameraService()
@@ -33,15 +35,33 @@ namespace WebController.Services
             });
         }
 
-        public void Capture(Camera cam)
+        public bool Capture(Camera cam)
         {
             cam.SetSetting(PropertyID.SaveTo, (int)SaveTo.Host);
             cam.SetCapacity(4096, int.MaxValue);
+
+            Console.WriteLine("Taking photo with current settings...");
+
+            CameraValue tv = TvValues.GetValue(cam.GetInt32Setting(PropertyID.Tv));
+            if (tv == TvValues.Bulb)
+                cam.TakePhotoBulb(2);
+            else
+                cam.TakePhoto();
+
+            WaitEvent.WaitOne();
+
+            if (!Error)
+            {
+                Console.WriteLine("Photo taken and saved");
+                return true;
+            }
+
+            return false;
         }
 
-        public bool OpenSession(Camera cam, Func<Camera, DownloadInfo, bool> downloadReady)
+        public bool OpenSession(Camera cam, Func<Camera, DownloadInfo, bool>? downloadReady = null)
         {
-            cam.DownloadReady += (Camera sender, DownloadInfo Info) => downloadReady.Invoke(sender, Info);
+            cam.DownloadReady += (Camera sender, DownloadInfo Info) => downloadReady?.Invoke(sender, Info);
             cam.OpenSession();
 
             Console.WriteLine($"Opened session with camera: {cam.DeviceName}");
